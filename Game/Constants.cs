b12624cb0d1fe94c4a06fd5b1352c8ed53249ad9
spaceorc <spaceorc@skbkontur.ru>
2017-05-17 17:44:22 +0500

    Add distances to Constants

--- Game/Constants.cs
+++ Game/Constants.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using Game.Types;
 
 namespace Game
@@ -8,5 +10,25 @@ namespace Game
 		public const int MAX_TRAY = 3;
 		public const int MAX_STORAGE = 10;
 		public const int MOLECULE_TYPE_COUNT = MoleculeType.E - MoleculeType.A + 1;
+
+		public static readonly Dictionary<Tuple<ModuleType, ModuleType>, int> distances = new Dictionary<Tuple<ModuleType, ModuleType>, int>
+		{
+			{ Tuple.Create(ModuleType.START_POS, ModuleType.SAMPLES), 2 },
+			{ Tuple.Create(ModuleType.START_POS, ModuleType.DIAGNOSIS), 2 },
+			{ Tuple.Create(ModuleType.START_POS, ModuleType.MOLECULES), 2 },
+			{ Tuple.Create(ModuleType.START_POS, ModuleType.LABORATORY), 2 },
+			{ Tuple.Create(ModuleType.SAMPLES, ModuleType.DIAGNOSIS), 3 },
+			{ Tuple.Create(ModuleType.SAMPLES, ModuleType.MOLECULES), 3 },
+			{ Tuple.Create(ModuleType.SAMPLES, ModuleType.LABORATORY), 3 },
+			{ Tuple.Create(ModuleType.DIAGNOSIS, ModuleType.SAMPLES), 3 },
+			{ Tuple.Create(ModuleType.DIAGNOSIS, ModuleType.MOLECULES), 3 },
+			{ Tuple.Create(ModuleType.DIAGNOSIS, ModuleType.LABORATORY), 4 },
+			{ Tuple.Create(ModuleType.MOLECULES, ModuleType.SAMPLES), 3 },
+			{ Tuple.Create(ModuleType.MOLECULES, ModuleType.DIAGNOSIS), 3 },
+			{ Tuple.Create(ModuleType.MOLECULES, ModuleType.LABORATORY), 3 },
+			{ Tuple.Create(ModuleType.LABORATORY, ModuleType.SAMPLES), 3 },
+			{ Tuple.Create(ModuleType.LABORATORY, ModuleType.DIAGNOSIS), 4 },
+			{ Tuple.Create(ModuleType.LABORATORY, ModuleType.MOLECULES), 3 },
+		};
 	}
 }
