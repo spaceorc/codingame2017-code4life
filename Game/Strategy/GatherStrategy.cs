d182e0714378b0dd936d04cd504ec2d21ba70260
spaceorc <spaceorc@skbkontur.ru>
2017-05-20 01:45:29 +0500

    Prevent while gathering

--- Game/Strategy/GatherStrategy.cs
+++ Game/Strategy/GatherStrategy.cs
@@ -61,13 +61,37 @@ namespace Game.Strategy
 			if (canGatherSample != null)
 			{
 				var turnsToGatherAndProduce = (producedSamples.Count + turnState.robot.eta + Constants.distances[Tuple.Create(turnState.robot.target, ModuleType.MOLECULES)]
-					+ turnState.robot.GetMoleculesToGather(canGatherSample, additionalExpertise, usedMolecules).totalCount + Constants.distances[Tuple.Create(turnState.robot.target, ModuleType.LABORATORY)]
-					+ 1) * 2;
+				                               + turnState.robot.GetMoleculesToGather(canGatherSample, additionalExpertise, usedMolecules).totalCount + Constants.distances[Tuple.Create(turnState.robot.target, ModuleType.LABORATORY)]
+				                               + 1)*2;
 				if (gameState.currentTurn + turnsToGatherAndProduce > Constants.TOTAL_TURNS && producedSamples.Any())
 					return new ProduceStrategy(gameState);
 
-				if (turnState.robot.GoTo(ModuleType.MOLECULES) == GoToResult.Arrived)
-					turnState.robot.Connect(GetMoleculeToGather(turnState, canGatherSample, additionalExpertise, usedMolecules));
+				if (turnState.robot.GoTo(ModuleType.MOLECULES) == GoToResult.OnTheWay)
+					return null;
+				var myMoleculesToGather = turnState.robot.GetMoleculesToGather(canGatherSample, additionalExpertise, usedMolecules);
+				var enemySamples = turnState.enemy.samples.Where(x => !turnState.enemy.CanProduce(x) && turnState.enemy.CanGather(turnState, x)).OrderByDescending(x => x.health).ToList();
+				foreach (var enemySample in enemySamples)
+				{
+					var enemyMoleculesToGather = turnState.enemy.GetMoleculesToGather(enemySample);
+					var totalMoleculesToGather = myMoleculesToGather.Add(enemyMoleculesToGather);
+					var overlayMolecules = totalMoleculesToGather.Subtract(turnState.available);
+					var maxi = -1;
+					var max = int.MinValue;
+					for (int i = 0; i < Constants.MOLECULE_TYPE_COUNT; i++)
+					{
+						if (overlayMolecules.counts[i] > max && myMoleculesToGather.counts[i] > 0)
+						{
+							max = overlayMolecules.counts[i];
+							maxi = i;
+						}
+					}
+					if (maxi != -1)
+					{
+						turnState.robot.Connect((MoleculeType)maxi);
+						return null;
+					}
+				}
+				turnState.robot.Connect(GetMoleculeToGather(turnState, canGatherSample, additionalExpertise, usedMolecules));
 				return null;
 			}
 
