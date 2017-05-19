68de0f34036983f95ef219433c63f0235afdcc70
spaceorc <spaceorc@skbkontur.ru>
2017-05-20 00:40:33 +0500

    Add Permutations

---
+++ Game/Strategy/Extensions.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using System.Linq;
+using Game.Types;
+
+namespace Game.Strategy
+{
+	public static class Extensions
+	{
+		public static List<List<Sample>> GetPermutations(this List<Sample> samples)
+		{
+			var result = new List<List<Sample>>();
+
+			var queue = new Queue<List<Sample>>();
+			foreach (var sample in samples)
+				queue.Enqueue(new List<Sample> {sample});
+			while (queue.Count > 0)
+			{
+				var current = queue.Dequeue();
+				if (current.Count == samples.Count)
+					result.Add(current);
+				else
+				{
+					foreach (var sample in samples)
+					{
+						if (!current.Contains(sample))
+						{
+							var newList = current.ToList();
+							newList.Add(sample);
+							queue.Enqueue(newList);
+						}
+					}
+				}
+			}
+			return result;
+		}
+	}
+}
---
+++ UnitTests/Extensions_Test.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using Game.Strategy;
+using Game.Types;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+	[TestFixture]
+	public class Extensions_Test
+	{
+		[Test]
+		public void GetPermutations_EmptyList_ReturnsEmptyPermutationsList()
+		{
+			new List<Sample>().GetPermutations().Should().BeEmpty();
+		}
+
+		[Test]
+		public void GetPermutations_OneElement_ReturnsOneElementPermutationsList()
+		{
+			var sample = new Sample(1, 1, 1, "A", 1, 1, 1, 1, 1, 1);
+			new List<Sample> {sample}.GetPermutations()
+				.ShouldBeEquivalentTo(
+					new List<List<Sample>>
+					{
+						new List<Sample> {sample}
+					});
+		}
+
+		[TestCase("AB", "AB", "BA")]
+		[TestCase("ABC", "ABC", "ACB", "BAC", "BCA", "CAB", "CBA")]
+		public void GetPermutations_ManyElements_ReturnsValidPermutationsList(string source, params string[] expected)
+		{
+			var samples = source.Select(x => new Sample(0, 0, 0, x.ToString(), 0, 0, 0, 0, 0, 0)).ToList();
+			var permutations = samples.GetPermutations().Select(x => string.Join("", x.Select(i => i.gainString))).ToList();
+			permutations.ShouldBeEquivalentTo(expected);
+		}
+	}
+}
