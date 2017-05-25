7cac2332fcf34c034d5cd45efe957237d0d68588
spaceorc <spaceorc@skbkontur.ru>
2017-05-25 20:56:27 +0500

    Remove trash

--- Game/Strategy/Extensions.cs
+++ Game/Strategy/Extensions.cs
@@ -6,35 +6,7 @@ namespace Game.Strategy
 {
 	public static class Extensions
 	{
-		public static List<List<Sample>> GetPermutations(this List<Sample> samples)
-		{
-			var result = new List<List<Sample>>();
-
-			var queue = new Queue<List<Sample>>();
-			foreach (var sample in samples)
-				queue.Enqueue(new List<Sample> {sample});
-			while (queue.Count > 0)
-			{
-				var current = queue.Dequeue();
-				if (current.Count == samples.Count)
-					result.Add(current);
-				else
-				{
-					foreach (var sample in samples)
-					{
-						if (!current.Contains(sample))
-						{
-							var newList = current.ToList();
-							newList.Add(sample);
-							queue.Enqueue(newList);
-						}
-					}
-				}
-			}
-			return result;
-		}
-
-		public static List<List<Sample>> GetVariants(this List<Sample> samples)
+	    public static List<List<Sample>> GetVariants(this List<Sample> samples)
 		{
 			var result = new List<List<Sample>>();
 			result.Add(new List<Sample>());
--- UnitTests/Extensions_Test.cs
+++ UnitTests/Extensions_Test.cs
@@ -11,52 +11,31 @@ namespace UnitTests
 	public class Extensions_Test
 	{
 		[Test]
-		public void GetPermutations_EmptyList_ReturnsEmptyPermutationsList()
+		public void GetVariants_EmptyList_ReturnsOneEmptyVariant()
 		{
-			new List<Sample>().GetPermutations().Should().BeEmpty();
+			new List<Sample>().GetVariants()
+		        .ShouldBeEquivalentTo(
+		            new List<List<Sample>>
+		            {
+		                new List<Sample>()
+		            });
 		}
 		
 		[Test]
-		public void GetPermutations_OneElement_ReturnsOneElementPermutationsList()
-		{
-			var sample = new Sample(1, 1, 1, "A", 1, 1, 1, 1, 1, 1);
-			new List<Sample> {sample}.GetPermutations()
-				.ShouldBeEquivalentTo(
-					new List<List<Sample>>
-					{
-						new List<Sample> {sample}
-					});
-		}
-
-		[TestCase("AB", "AB", "BA")]
-		[TestCase("ABC", "ABC", "ACB", "BAC", "BCA", "CAB", "CBA")]
-		public void GetPermutations_ManyElements_ReturnsValidPermutationsList(string source, params string[] expected)
-		{
-			var samples = source.Select(x => new Sample(0, 0, 0, x.ToString(), 0, 0, 0, 0, 0, 0)).ToList();
-			var permutations = samples.GetPermutations().Select(x => string.Join("", x.Select(i => i.gainString))).ToList();
-			permutations.ShouldBeEquivalentTo(expected);
-		}
-
-		[Test]
-		public void GetVariants_EmptyList_ReturnsEmptyPermutationsList()
-		{
-			new List<Sample>().GetVariants().Should().BeEmpty();
-		}
-		
-		[Test]
-		public void GetVariants_OneElement_ReturnsOneElementPermutationsList()
+		public void GetVariants_OneElement_ReturnsValidVariantsList()
 		{
 			var sample = new Sample(1, 1, 1, "A", 1, 1, 1, 1, 1, 1);
 			new List<Sample> {sample}.GetVariants()
 				.ShouldBeEquivalentTo(
 					new List<List<Sample>>
 					{
+						new List<Sample>(),
 						new List<Sample> {sample}
 					});
 		}
 
-		[TestCase("AB", "AB", "BA", "A", "B")]
-		[TestCase("ABC", "ABC", "ACB", "BAC", "BCA", "CAB", "CBA", "AB", "AC", "BA", "BC", "CA", "CB", "A", "B", "C")]
+		[TestCase("AB", "AB", "BA", "A", "B", "")]
+		[TestCase("ABC", "ABC", "ACB", "BAC", "BCA", "CAB", "CBA", "AB", "AC", "BA", "BC", "CA", "CB", "A", "B", "C", "")]
 		public void GetVariants_ManyElements_ReturnsValidVariantsList(string source, params string[] expected)
 		{
 			var samples = source.Select(x => new Sample(0, 0, 0, x.ToString(), 0, 0, 0, 0, 0, 0)).ToList();
